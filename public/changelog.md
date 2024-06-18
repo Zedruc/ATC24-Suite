d9b6c0c838089347336ab5adfba829d8f367085e
Zedruc <69540929+Zedruc@users.noreply.github.com>
2024-06-18 20:24:06 +0200

    FIX Firefox warning

--- public/changelog.md
+++ public/changelog.md
@@ -1,10 +1,15 @@
 <div id="changelog"></div>
 
-# ATIS PDC Formulation Change
+# Networking Fix + Compatibility Notice
 
-**🔧 Changes**  
+**🔧 Website**  
 
-* Changed the ATIS from saying "CPDLC" to "PDC"
+* The warning "Firefox is NOT compatible with this website at all at all" now actually works. (Don't use Firefox, it will cause you to not be able to use the suite 80% of the time)
+
+**🧰 Internal**
+
+* Fixed API server erroring after the Database had huge troubles adapting to a network configuration change (The cable moved one port to the right 🤡)
+* Fixed API server trying to analyze unrelated errors when users try to authenticate without Database connection
 
 Happy Controlling
 \- Zedruc
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,10 +13,10 @@ if (window.location.hostname == 'zedruc.net') {
   console.warn = noOp;
 } */
 
-if (!window?.navigator?.clipboard?.readText) {
+if (window?.navigator?.userAgent.toLowerCase().includes('firefox')) {
   notificationQueue.queue({
     title: 'Important!',
-    html: `It seems you are using a browser like Firefox, which doesn't support features required for this program to function properly.<br/>Consider using Chrome, as this is what the ATC24-Suite was developed for.`,
+    html: `It seems you are using a browser like Firefox, which doesn't support features required for this program to function properly.<br/><b>Firefox uses may increasingly experience issues with the suite thinking you are still logged in because of Firefox not closing connections properly.</b><br/> Consider using Chrome, as this is what the ATC24-Suite was developed for.`,
     icon: 'info',
   });
 }
